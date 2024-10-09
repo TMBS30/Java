5dffe37a77a46416f4f57c530f6080e6855a0c61
Thiago Monteiro <thgmonty@gmail.com>
2024-10-08 22:30:40 -0300

    DescExclusao de classes e comentarios sem utilidades

--- ClassJava_07/src/TrataErro.java
+++ ClassJava_07/src/TrataErro.java
@@ -3,17 +3,6 @@ import java.util.Scanner;
 public class TrataErro {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        /*try {
-            System.out.print("Digite um valor inteiro..:");
-            int numero1 = s.nextInt();
-            System.out.print("Digite um valor inteiro..:");
-            int numero2 = s.nextInt();
-
-            System.out.println(numero1+ " + " + numero2 + " = " + (numero1+numero2));
-        } catch (Exception ex) {
-            System.out.println("ERRO - Valor digitado nao e um numero inteiro!");
-        }*/
-
         try {
             System.out.print("Digite seu nome..:");
             String nome = scanner.next();
